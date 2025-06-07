fb51977eeba9b5c5622bd0d04b4f23b9312fd3f4
Lucas Souza <ls18017@gmail.com>
2025-06-07 17:36:09 -0300

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router";
 
 import { AppLayout } from './shared/layout/AppLayout';
-
+import { Login } from './pages/public/Login';
 import { Detail } from './pages/Detail';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
 
 
 export function App() {
+  const isAuthenticated = false;
+
 
   return (
     <BrowserRouter>
-      <AppLayout>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/sobre' element={<About />} />
-          <Route path='/detalhe/:id' element={<Detail />} />
+      {isAuthenticated && (
+        <AppLayout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/sobre' element={<About />} />
+            <Route path='/detalhe/:id' element={<Detail />} />
 
-          <Route path='*' element={<Navigate to='/' />} />
+            <Route path='*' element={<Navigate to='/' />} />
+          </Routes>
+        </AppLayout>
+      )}
+      {!isAuthenticated && (
+        <Routes>
+          <Route path='*' element={<Login />} />
         </Routes>
-      </AppLayout>
+      )}
     </BrowserRouter>
   )
 }
