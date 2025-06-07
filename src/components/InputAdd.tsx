abf7193a455b260fcf72210c3f218ca6e8bf8f33
Lucas Souza <ls18017@gmail.com>
2025-06-07 16:41:23 -0300

    feat: add useRef in InputAdd

--- src/components/InputAdd.tsx
+++ src/components/InputAdd.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import InputAddStyles from './InputAdd.module.css';
 
@@ -7,12 +7,16 @@ interface IInputAddProps {
   onAdd(value: string): void;
 }
 export const InputAdd = (props: IInputAddProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+
   const [value, setValue] = useState('');
 
 
   const handleAdd = () => {
     props.onAdd(value);
     setValue('');
+    inputRef.current?.focus();
   }
 
 
@@ -20,6 +24,7 @@ export const InputAdd = (props: IInputAddProps) => {
     <div className={InputAddStyles.Container}>
       <input
         value={value}
+        ref={inputRef}
         className={InputAddStyles.Input}
         onChange={(e) => setValue(e.target.value)}
       />
