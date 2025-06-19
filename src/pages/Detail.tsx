67b6d8944fc2ed8fc5ef966e54bc0db3c2b59937
Lucas Souza <ls18017@gmail.com>
2025-06-19 08:19:55 -0300

    refactor: remove useSearchParams

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,16 +1,15 @@
-import { useParams, useSearchParams } from 'react-router';
+import { useParams } from 'react-router';
 
 import { PageLayout } from '../shared/layout/page-layout/PageLayout';
 
 
 export const Detail = () => {
-  const [searchParams] = useSearchParams();
   const { id } = useParams();
 
 
   return (
     <PageLayout title='Detalhes'>
-      Detail {id} {searchParams.get('filter')}
+      Detail {id === 'adicionar' ? 'Adicionando' : 'Editando'}
     </PageLayout>
   );
 };
