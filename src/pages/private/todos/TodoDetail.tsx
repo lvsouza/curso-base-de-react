d8b5b379362a6f4219204591a2899f6de4922307
Lucas Souza <ls18017@gmail.com>
2025-06-19 09:35:38 -0300

    feat: add react-hook-form

--- src/pages/private/todos/TodoDetail.tsx
+++ src/pages/private/todos/TodoDetail.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router';
+import { useForm } from 'react-hook-form';
 
 import { PageLayout } from '../../../shared/layout/page-layout/PageLayout';
 
@@ -6,10 +7,26 @@ import { PageLayout } from '../../../shared/layout/page-layout/PageLayout';
 export const TodoDetail = () => {
   const { id } = useParams();
 
+  const { register, handleSubmit, } = useForm({
+    defaultValues: {
+      label: 'Default',
+      description: 'Default',
+      complete: false,
+    }
+  });
+
 
   return (
     <PageLayout title='Detalhes'>
-      Detail {id === 'adicionar' ? 'Adicionando' : 'Editando'}
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <input {...register('label')} />
+        <input {...register('description')} />
+        <input type='checkbox' {...register('complete')} />
+
+        <button type='submit'>
+          Submit
+        </button>
+      </form>
     </PageLayout>
   );
 };
